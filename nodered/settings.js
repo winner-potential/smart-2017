eefbcb1c04fa24889c95acbef5d4ece674a7a559
Sebastian Apel <Seb@dyn-0a230c36.fmi.uni-jena.de>
2017-06-14 16:03:19 +0200

    fixed nodered and camel namings

--- nodered/settings.js
+++ nodered/settings.js
@@ -191,7 +191,7 @@ module.exports = {
             // info - record information about the general running of the application + warn + error + fatal errors
             // debug - record information which is more verbose than info + info + warn + error + fatal errors
             // trace - record very detailed logging + debug + info + warn + error + fatal errors
-            level: "info",
+            level: "warn",
             // Whether or not to include metric events in the log output
             metrics: false,
             // Whether or not to include audit events in the log output
