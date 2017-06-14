93093fff01f88d4f34d0956940f35a0271e35c28
Florian hertrampf <florian.hertrampf@uni-jena.de>
2017-06-14 11:00:22 +0200

    Workaround for user rights on Debian

--- wso2/wso2bin/startwso2.sh
+++ wso2/wso2bin/startwso2.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-export JAVA_HOME=/mnt/jdk-7u80 && /mnt/wso2cep-4.2.0/bin/wso2server.sh
+
+export JAVA_HOME=/mnt/jdk-7u80
+/usr/local/bin/cpu "java" &
+/usr/local/bin/mem "Bootstrap" &
+/mnt/wso2cep-4.2.0/bin/wso2server.sh
