66db40047a2253777a27ed76f9c1a5c71ec79bd7
Florian Hertrampf <florian.hertrampf@uni-jena.de>
2018-02-09 17:24:16 +0100

    Added reworking script for incoming and outgoing

---
+++ plotting/Process_In_Out/main.cpp
@@ -0,0 +1,144 @@
+#include <QCoreApplication>
+#include <QFile>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QTextStream>
+#include <QDebug>
+
+QString getID(QString s)
+{
+    QJsonDocument d = QJsonDocument::fromJson(s.toUtf8());
+    QJsonObject object = d.object();
+    if(object["content"].isNull())
+    {
+        return object["id"].toString();
+    }
+    else
+    {
+        return object["content"].toObject()["id"].toString();
+    }
+}
+
+QString getCat(QString s)
+{
+    QStringList l = s.split("-");
+    return QString(l[0]+"-"+l[1]);
+}
+
+QString getNaN(double d)
+{
+    if(d!=0)
+    {
+        return QString::number(d,'g',20);
+    }
+    else
+    {
+        return "NaN";
+    }
+}
+
+//tags -> DB
+// no tags -> Avg
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    if(argc>=4)
+    {
+        QFile incoming(QCoreApplication::arguments().at(1));
+        QFile outgoing(QCoreApplication::arguments().at(2));
+        QFile errorfile(QCoreApplication::arguments().at(3)+"_errors");
+        QMap<QString,QVector<double>> resultmap;
+
+        QString errors("");
+
+        QJsonObject object;
+        QString line("");
+        long errorcounter = 0;
+        if(outgoing.open(QIODevice::ReadOnly))
+        {
+            QTextStream in(&outgoing);
+            while (!in.atEnd())
+            {
+                line = in.readLine();
+                QVector<double> v(3);
+                object = QJsonDocument::fromJson(line.toUtf8()).object();
+                v[0] = object["time"].toDouble();
+                if(object["error"].isNull() && getID(line)!="")
+                {
+                    resultmap.insert(getID(line),v);
+                }
+                else
+                {
+                    if(!object["error"].isNull() &&
+                       !object["error"].toObject()["id"].isNull())
+                    {
+                        resultmap.insert(object["error"].toObject()["id"]
+                                .toString(),v);
+                    }
+                    errors.append(line+"\n");
+                    errorcounter++;
+                }
+            }
+            outgoing.close();
+        }
+        qInfo() << "Read outgoing " << resultmap.count() << "with errors" <<
+                   errorcounter;
+        if(incoming.open(QIODevice::ReadOnly))
+        {
+            QTextStream in(&incoming);
+            while (!in.atEnd())
+            {
+                line = in.readLine();
+                object = QJsonDocument::fromJson(line.toUtf8()).object();
+                if(object["content"].toObject()["tags"].isNull())
+                {
+                    //avg
+                    resultmap[object["content"].toObject()["id"].toString()][2]
+                    = object["time"].toDouble();
+                }
+                else
+                {
+                    //db
+                    resultmap[object["content"].toObject()["id"].toString()][1]
+                    = object["time"].toDouble();
+                }
+            }
+            incoming.close();
+        }
+        qInfo() << "Read and inserted incoming " << resultmap.size();
+
+        QMapIterator<QString, QVector<double>> i(resultmap);
+        while (i.hasNext())
+        {
+            i.next();
+            QFile out(QCoreApplication::arguments().at(3)+"_"+getCat(i.key()));
+            if(!out.exists())
+            {
+                if(out.open(QIODevice::Append))
+                {
+                    out.write("id out dbin avgin\n");
+                    out.close();
+                }
+            }
+            if(out.open(QIODevice::Append))
+            {
+                QTextStream outstream(&out);
+                outstream << i.key() << " "
+                << getNaN(i.value().at(0)) << " "
+                << getNaN(i.value().at(1)) << " "
+                << getNaN(i.value().at(2)) << "\n";
+                outstream.flush();
+                out.close();
+            }
+        }
+        if(errorfile.open(QIODevice::WriteOnly))
+        {
+            QTextStream in(&errorfile);
+            in << errors;
+            in.flush();
+            errorfile.close();
+        }
+    }
+    return 0;
+}
---
+++ plotting/scripts/plottimedifferences.plt
@@ -0,0 +1,53 @@
+reset
+
+set datafile missing "NaN"
+factorx = 1
+factory = 1
+#stepsizex = 100
+#stepsizey = 20
+
+unset border
+set border 3
+
+set key right top
+set key box linetype 1 linecolor '#000000' linewidth 2
+set key width +1
+set key spacing 1.5
+
+set terminal pdf size 10cm,6cm enhanced font 'Monospace,8' linewidth 1 rounded dashed
+set output outputfilename
+
+set xlabel "time [ms]"
+set ylabel "response time [ms]"
+set datafile separator " "
+set decimalsign '.'
+
+xoffsets = ""
+do for [i=1:words(files)] {
+    stats word(files,i) using ($2*factorx)
+    xoffsets = xoffsets." ".sprintf("%f",STATS_min)
+}
+
+set xtics nomirror
+#set grid xtics
+#set xtics 0,1000,3000
+set xrange [-1:]
+set ytics nomirror
+set grid ytics
+#set ytics 0,20,120
+set yrange [-10:]
+
+
+myfont = "Monospace,10"
+
+plot for [i=1:words(files)] word(files, i) \
+using ($2*factorx-(word(xoffsets,i)+0)):($3-$2) \
+title "db" with linespoints \
+pointtype word(symbols,i) lc rgb "black" font myfont, \
+word(files, i) \
+using ($2*factorx-(word(xoffsets,i)+0)):($4-$2) \
+title "avg" with linespoints \
+pointtype word(symbols,i+1) lc rgb "black" font myfont
+
+# pause -1
+
